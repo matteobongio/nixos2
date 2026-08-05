edf5d3fcb5f0fac4cddc69507f3c963cc07d6eb7
Matteo Bongiovanni <matteobongiovanni@proton.me>
2026-08-05 14:33:03 +0200

    remove nvidia env vars

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -9,13 +9,13 @@
 
     # TODO: check
     hardware.nvidia.modesetting.enable = true;
-    environment.sessionVariables = {
-      LIBVA_DRIVER_NAME = "nvidia";
-      XDG_SESSION_TYPE = "wayland";
-      GBM_BACKEND = "nvidia-drm";
-      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-      NVD_BACKEND = "direct";
-    };
+    # environment.sessionVariables = {
+    #   LIBVA_DRIVER_NAME = "nvidia";
+    #   XDG_SESSION_TYPE = "wayland";
+    #   GBM_BACKEND = "nvidia-drm";
+    #   __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    #   NVD_BACKEND = "direct";
+    # };
 
   };
 }
